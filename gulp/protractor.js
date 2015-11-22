16926ab3959f3811f9414e72c84682b09cd958d8
Calvin McEachron <calvin@digitil.me>
2015-11-21 20:58:33 -0500

    Add gulp-live server and start it before protractor tests run

--- gulp/protractor.js
+++ gulp/protractor.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp');
+var gls = require('gulp-live-server');
 var protractor = require('gulp-angular-protractor');
 
-gulp.task('protractor', function () {
+var server;
+
+gulp.task('serve-testapp', function() {
+    server = gls([gls.script, 'testapp', 8888], undefined, false);
+    server.start();
+});
+
+gulp.task('protractor', ['serve-testapp'], function () {
     var config = {
             configFile: 'test/integration/conf.js',
             autoStartStopServer: true,
@@ -10,5 +18,11 @@ gulp.task('protractor', function () {
 
     return gulp.src(['test/integration/*.spec.js'])
         .pipe(protractor(config))
-        .on('error', function (e) { throw e });
+        .on('end', function () {
+            server.stop();
+        })
+        .on('error', function (e) {
+            server.stop();
+            throw e;
+        });
 });
--- test/integration/conf.js
+++ test/integration/conf.js
@@ -1,10 +1,7 @@
-var host = process.env.HTTP_HOST || 'localhost';
-var port = process.env.HTTP_PORT || '8081';
-
 exports.config = {
     specs: ['**/*.spec.js'],
 
-    baseUrl: 'http://' + host + ':' + port,
+    baseUrl: 'http://localhost:8888',
 
     jasmineNodeOpts: {
         isVerbose: true,
