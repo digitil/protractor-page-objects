1df9f28009b021a66b062a4bcdbed80a64a41f9f
Calvin McEachron <calvin@digitil.me>
2015-12-20 09:17:36 -0500

    Run `publish` task serially

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
+var sequence = require('run-sequence');
 require('./gulp');
 
 gulp.task('lint', ['jshint', 'jscs']);
 gulp.task('test', ['mocha', 'protractor']);
 gulp.task('default', ['lint', 'test']);
 
-gulp.task('publish', ['default', 'jsdoc', 'gh-pages']);
+gulp.task('publish', function() {
+	sequence('default', 'jsdoc', 'gh-pages');
+});
